470c5cde10828d17ae34e7c87c21c6cb2819aa73
Kam Wing Yiu <ctom3560@gmail.com>
2026-08-21 13:18:13 +0800

    week 3.4: implement watermark

--- mini-lsm-starter/src/mvcc/watermark.rs
+++ mini-lsm-starter/src/mvcc/watermark.rs
@@ -12,12 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
-
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, VecDeque, btree_map::Entry};
 
 pub struct Watermark {
+    /// Queue storing unique active timestamps in chronological order
+    deque: VecDeque<u64>,
+    /// HashMap mapping timestamp -> active_reader_count for O(1) removals
     readers: BTreeMap<u64, usize>,
 }
 
@@ -30,19 +30,40 @@ impl Default for Watermark {
 impl Watermark {
     pub fn new() -> Self {
         Self {
+            deque: VecDeque::new(),
             readers: BTreeMap::new(),
         }
     }
 
-    pub fn add_reader(&mut self, ts: u64) {}
+    pub fn add_reader(&mut self, ts: u64) {
+        let count = self.readers.entry(ts).or_insert(0);
+        if *count == 0 {
+            self.deque.push_back(ts);
+        }
+        *count += 1;
+    }
 
-    pub fn remove_reader(&mut self, ts: u64) {}
+    pub fn remove_reader(&mut self, ts: u64) {
+        if let Entry::Occupied(mut entry) = self.readers.entry(ts) {
+            *entry.get_mut() -= 1;
+            if *entry.get() == 0 {
+                entry.remove();
+            }
+        }
+
+        while let Some(&front_ts) = self.q.front() {
+            if self.readers.contains_key(&front_ts) {
+                break;
+            }
+            self.deque.pop_front();
+        }
+    }
 
     pub fn num_retained_snapshots(&self) -> usize {
         self.readers.len()
     }
 
     pub fn watermark(&self) -> Option<u64> {
-        Some(0)
+        self.deque.front().copied()
     }
 }
